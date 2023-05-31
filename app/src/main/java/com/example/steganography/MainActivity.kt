424790056f8e480aa97c9a086ea8d86b0e5bfbc5
Mila <gorbunova.milasha@gmail.com>
2023-05-31 15:55:35 +0300

    initial commit

---
+++ app/src/main/java/com/example/steganography/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.steganography
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
