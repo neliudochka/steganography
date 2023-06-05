5d0ef81f6f65bef6cc8b6c121afc1f90ebe9601f
Mila <gorbunova.milasha@gmail.com>
2023-06-05 18:51:17 +0300

    Rewritten image selection with Activity Result API part
    Due to the fact, that using onActivityResult() is a bad  practice,
    I rewrote the selection of the photo from the gallery with Activity Result API.

--- app/src/main/java/com/example/steganography/fragments/decode/DecodeSetupFragment.kt
+++ app/src/main/java/com/example/steganography/fragments/decode/DecodeSetupFragment.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.steganography.MainActivity
@@ -16,31 +17,31 @@ class DecodeSetupFragment : Fragment(R.layout.fragment_decode_setup){
     private lateinit var binding: FragmentDecodeSetupBinding
     private var title = "Decode: setup"
 
-    private val pickImage = 100
-    private var imageUri: Uri? = null
+    private val previewImage by lazy { binding.image }
+    private val selectImageFromGalleryResult = registerForActivityResult(
+        ActivityResultContracts
+        .GetContent()) { uri: Uri? -> uri?.let { previewImage.setImageURI(uri) }
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDecodeSetupBinding.bind(view)
         binding.start.setOnClickListener { onStartPressed(view) }
         (activity as MainActivity).supportActionBar?.setTitle(title)
 
-        initButtonsListeners()
+        initLoadPictureButton()
     }
 
     private fun onStartPressed(view: View) {
         view.findNavController().navigate(R.id.action_decodeSetupFragment_to_decodeResultFragment)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == pickImage) {
-            imageUri = data?.data
-            binding.image.setImageURI(imageUri)
-        }
-    }
-    private fun initButtonsListeners() {
+    private fun initLoadPictureButton() {
         binding.buttonLoadPicture.setOnClickListener {
-            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-            startActivityForResult(gallery, pickImage)  }
+            selectImageFromGallery() }
     }
+
+    private fun selectImageFromGallery() {
+        selectImageFromGalleryResult.launch("image/*")
+    }
+
 }
--- app/src/main/java/com/example/steganography/fragments/encode/EncodeSetupFragment.kt
+++ app/src/main/java/com/example/steganography/fragments/encode/EncodeSetupFragment.kt
@@ -6,6 +6,10 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.steganography.MainActivity
@@ -16,32 +20,33 @@ class EncodeSetupFragment : Fragment(R.layout.fragment_encode_setup){
     private lateinit var binding: FragmentEncodeSetupBinding
     private var title = "Encode: setup"
 
-    private val pickImage = 100
-    private var imageUri: Uri? = null
-
+    private val previewImage by lazy { binding.image }
+    private val selectImageFromGalleryResult = registerForActivityResult(ActivityResultContracts
+        .GetContent()) { uri: Uri? -> uri?.let { previewImage.setImageURI(uri) }
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentEncodeSetupBinding.bind(view)
         binding.start.setOnClickListener { onStartPressed(view) }
         (activity as MainActivity ).supportActionBar?.setTitle(title)
 
-        initButtonsListeners()
+        initLoadPictureButton()
     }
     private fun onStartPressed(view: View) {
         view.findNavController().navigate(R.id.action_encodeSetupFragment_to_encodeResultFragment)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK && requestCode == pickImage) {
-            imageUri = data?.data
-            binding.image.setImageURI(imageUri)
-        }
+    private fun initLoadPictureButton() {
+        binding.buttonLoadPicture.setOnClickListener {
+            selectImageFromGallery() }
     }
 
-    private fun initButtonsListeners() {
-        binding.buttonLoadPicture.setOnClickListener {
-            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-            startActivityForResult(gallery, pickImage)  }
+    private fun selectImageFromGallery() {
+        selectImageFromGalleryResult.launch("image/*")
     }
+
+
 }
+
+
+
